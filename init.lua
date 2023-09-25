cc779b1286b9df3ee00908893869be4f96c3fd87
Kade Dean <77413927+kaderator2@users.noreply.github.com>
2023-09-25 11:41:19 -0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,3 +1,7 @@
-require("chade.remap")
+vim.g.mapleader = " "
 require("lazy-config")
+require("chade.remap")
 require("chade.set")
+require("mason").setup()
+require("mason-lspconfig").setup()
+require("lspconfig")
